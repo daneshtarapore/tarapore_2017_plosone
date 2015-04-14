31b5f16778f5216f74f26cad722e50c8616af244
Danesh Tarapore <daneshtarapore@gmail.com>
2015-04-14 18:53:46 +0100

    update

---
+++ controllers/epuck_hom_swarm/epuck_hom_swarm.cpp
@@ -0,0 +1,287 @@
+/* Include the controller definition */
+#include "epuck_hom_swarm.h"
+
+/* Function definitions for XML parsing */
+#include <argos3/core/utility/configuration/argos_configuration.h>
+
+/* 2D vector definition */
+#include <argos3/core/utility/math/vector2.h>
+
+/* Logging */
+#include <argos3/core/utility/logging/argos_log.h>
+
+/****************************************/
+/****************************************/
+
+CBehavior::SensoryData CBehavior::m_sSensoryData;
+CBehavior::RobotData CBehavior::m_sRobotData;
+
+/****************************************/
+/****************************************/
+
+CEPuckHomSwarm::ExperimentToRun::ExperimentToRun() :
+    SBehavior(SWARM_AGGREGATION),
+    FBehavior(FAULT_NONE),
+    id_FaultyRobotInSwarm("-1") {}
+
+
+void CEPuckHomSwarm::ExperimentToRun::Init(TConfigurationNode& t_node)
+{
+    std::string swarmbehav, errorbehav;
+
+    try
+    {
+        GetNodeAttribute(t_node, "swarm_behavior", swarmbehav);
+        GetNodeAttribute(t_node, "fault_behavior", errorbehav);
+        GetNodeAttribute(t_node, "id_faulty_robot", id_FaultyRobotInSwarm);
+    }
+    catch(CARGoSException& ex)
+            THROW_ARGOSEXCEPTION_NESTED("Error initializing type of experiment to run, and fault to simulate.", ex);
+
+    if (swarmbehav.compare("SWARM_AGGREGATION") == 0)
+        SBehavior = SWARM_AGGREGATION;
+    else if (swarmbehav.compare("SWARM_DISPERSION") == 0)
+        SBehavior = SWARM_DISPERSION;
+    else if (swarmbehav.compare("SWARM_HOMING") == 0)
+        SBehavior = SWARM_HOMING;
+    else
+    {
+        std::cerr << "invalid swarm behavior";
+        exit(-1);
+    }
+
+    if (errorbehav.compare("FAULT_NONE") == 0)
+        FBehavior = FAULT_NONE;
+    else if  (errorbehav.compare("FAULT_STRAIGHTLINE") == 0)
+        FBehavior = FAULT_STRAIGHTLINE;
+    else if  (errorbehav.compare("FAULT_RANDOMWALK") == 0)
+        FBehavior = FAULT_RANDOMWALK;
+    else if  (errorbehav.compare("FAULT_CIRCLE") == 0)
+        FBehavior = FAULT_CIRCLE;
+    else if  (errorbehav.compare("FAULT_STOP") == 0)
+        FBehavior = FAULT_STOP;
+    else
+    {
+        std::cerr << "invalid fault behavior";
+        exit(-1);
+    }
+
+}
+
+/****************************************/
+/****************************************/
+
+void CEPuckHomSwarm::SWheelTurningParams::Init(TConfigurationNode& t_node)
+{
+    try
+    {
+        GetNodeAttribute(t_node, "max_speed", MaxSpeed);
+    }
+    catch(CARGoSException& ex)
+            THROW_ARGOSEXCEPTION_NESTED("Error initializing controller wheel turning parameters.", ex);
+}
+
+/****************************************/
+/****************************************/
+
+CEPuckHomSwarm::CEPuckHomSwarm() :
+    m_pcWheels(NULL),
+    m_pcLEDs(NULL),
+    m_pcRABA(NULL),
+    m_pcRABS(NULL),
+    m_pcProximity(NULL),
+    m_pcRNG(NULL),
+    b_damagedrobot(false) {}
+
+/****************************************/
+/****************************************/
+
+void CEPuckHomSwarm::Init(TConfigurationNode& t_node)
+{
+    try
+    {
+        /*
+       * Initialize sensors/actuators
+       */
+        m_pcWheels    = GetActuator<CCI_DifferentialSteeringActuator>("differential_steering");
+        m_pcLEDs      = GetActuator<CCI_LEDsActuator                >("leds"                 );
+        m_pcRABA      = GetActuator<CCI_RangeAndBearingActuator     >("range_and_bearing"    );
+        m_pcRABS      = GetSensor  <CCI_RangeAndBearingSensor       >("range_and_bearing"    );
+        m_pcProximity = GetSensor  <CCI_EPuckProximitySensor        >("epuck_proximity"    );
+
+        /*
+       * Parse XML parameters
+       */
+        /* Experiment to run */
+        m_sExpRun.Init(GetNode(t_node, "experiment_run"));
+        /* Wheel turning */
+        m_sWheelTurningParams.Init(GetNode(t_node, "wheel_turning"));
+    }
+    catch(CARGoSException& ex)
+            THROW_ARGOSEXCEPTION_NESTED("Error initializing the e-puck hom_swarm controller for robot \"" << GetId() << "\"", ex);
+
+    /*
+    * Initialize other stuff
+    */
+    /* Create a random number generator. We use the 'argos' category so
+      that creation, reset, seeding and cleanup are managed by ARGoS. */
+    m_pcRNG = CRandom::CreateRNG("argos");
+
+    Reset();
+
+    CBehavior::m_sRobotData.MaxSpeed = m_sWheelTurningParams.MaxSpeed;
+    CBehavior::m_sRobotData.iterations_per_second = 10.0f * 10.0f; /*10 ticks per second, and 10 interations per tick so dt=0.01s*/
+    CBehavior::m_sRobotData.seconds_per_iterations * 1.0f / CBehavior::m_sRobotData.iterations_per_second;
+    CBehavior::m_sRobotData.HALF_INTERWHEEL_DISTANCE = 0.053f * 0.5f;
+    CBehavior::m_sRobotData.INTERWHEEL_DISTANCE  = 0.053f;
+    CBehavior::m_sRobotData.WHEEL_RADIUS = 0.0205f;
+
+    CBehavior::m_sRobotData.m_cNoTurnOnAngleThreshold   = ToRadians(CDegrees(10.0f)); //10.0 - straight to food spot; 35.0 spiral to food spot
+    CBehavior::m_sRobotData.m_cSoftTurnOnAngleThreshold = ToRadians(CDegrees(70.0f));
+
+    if(this->GetId().compare("ep"+m_sExpRun.id_FaultyRobotInSwarm) == 0)
+        b_damagedrobot = true;
+}
+
+/****************************************/
+/****************************************/
+
+void CEPuckHomSwarm::ControlStep()
+{
+    if(b_damagedrobot && (m_sExpRun.FBehavior == ExperimentToRun::FAULT_STRAIGHTLINE ||
+                          m_sExpRun.FBehavior == ExperimentToRun::FAULT_RANDOMWALK ||
+                          m_sExpRun.FBehavior == ExperimentToRun::FAULT_CIRCLE ||
+                          m_sExpRun.FBehavior == ExperimentToRun::FAULT_STOP))
+        RunGeneralFaults();
+
+    else if(m_sExpRun.SBehavior == ExperimentToRun::SWARM_AGGREGATION ||
+            m_sExpRun.SBehavior == ExperimentToRun::SWARM_DISPERSION ||
+            m_sExpRun.SBehavior == ExperimentToRun::SWARM_HOMING)
+        RunHomogeneousSwarmExperiment();
+
+
+    CBehavior::m_sSensoryData.SetSensoryData(m_pcRNG, m_pcProximity->GetReadings(), m_pcRABS->GetReadings());
+
+    Real leftSpeed = 0.0, rightSpeed = 0.0;
+    bool bControlTaken = false;
+    for (TBehaviorVectorIterator i = m_vecBehaviors.begin(); i != m_vecBehaviors.end(); i++)
+    {
+        if (!bControlTaken)
+        {
+            bControlTaken = (*i)->TakeControl();
+            if (bControlTaken)
+            {
+                (*i)->Action(leftSpeed, rightSpeed);
+            }
+        } else
+            (*i)->Suppress();
+    }
+
+    m_pcWheels->SetLinearVelocity(leftSpeed, rightSpeed); // in cm/s
+}
+
+/****************************************/
+/****************************************/
+
+void CEPuckHomSwarm::RunGeneralFaults()
+{
+    m_pcLEDs->SetAllColors(CColor::RED);
+
+    m_vecBehaviors.clear();
+    if(m_sExpRun.FBehavior == ExperimentToRun::FAULT_STRAIGHTLINE)
+    {
+         CRandomWalkBehavior* pcStraightLineBehavior = new CRandomWalkBehavior(0.0f);
+         m_vecBehaviors.push_back(pcStraightLineBehavior);
+    }
+    else if (m_sExpRun.FBehavior == ExperimentToRun::FAULT_RANDOMWALK)
+    {
+        CRandomWalkBehavior* pcRandomWalkBehavior = new CRandomWalkBehavior(0.05f);
+        m_vecBehaviors.push_back(pcRandomWalkBehavior);
+    }
+
+    else if (m_sExpRun.FBehavior == ExperimentToRun::FAULT_CIRCLE)
+    {
+        CCircleBehavior* pcCircleBehavior = new CCircleBehavior();
+        m_vecBehaviors.push_back(pcCircleBehavior);
+
+    }
+    else //m_sExpRun.FBehavior == ExperimentToRun::FAULT_STOP
+    {}
+}
+
+/****************************************/
+/****************************************/
+
+void CEPuckHomSwarm::RunHomogeneousSwarmExperiment()
+{
+    m_vecBehaviors.clear();
+
+    if(m_sExpRun.SBehavior == ExperimentToRun::SWARM_AGGREGATION)
+    {
+        CDisperseBehavior* pcDisperseBehavior = new CDisperseBehavior(0.1f, ToRadians(CDegrees(5.0f)));    // 0.1f reflects a distance of about 4.5cm
+        m_vecBehaviors.push_back(pcDisperseBehavior);
+
+        CAggregateBehavior* pcAggregateBehavior = new CAggregateBehavior(30.0f); //range threshold in cm
+        m_vecBehaviors.push_back(pcAggregateBehavior);
+
+        CRandomWalkBehavior* pcRandomWalkBehavior = new CRandomWalkBehavior(0.05f);
+        m_vecBehaviors.push_back(pcRandomWalkBehavior);
+    }
+
+    else if(m_sExpRun.SBehavior == ExperimentToRun::SWARM_DISPERSION)
+    {
+        CDisperseBehavior* pcDisperseBehavior = new CDisperseBehavior(0.1f, ToRadians(CDegrees(5.0f)));
+        m_vecBehaviors.push_back(pcDisperseBehavior);
+
+        CRandomWalkBehavior* pcRandomWalkBehavior = new CRandomWalkBehavior(0.05f);
+        m_vecBehaviors.push_back(pcRandomWalkBehavior);
+    }
+
+    else if(m_sExpRun.SBehavior == ExperimentToRun::SWARM_HOMING)
+    {
+        if(this->GetId().compare("ep0") == 0)
+        {
+            // ep0 is the beacon robot
+            /* Sends out data with RABS that you are a beacon. Neighbouring robots will use this data to home in on your position */
+            unsigned BEACON_ESTABLISHED = 3u;
+            m_pcRABA->SetData(0, BEACON_ESTABLISHED);
+            m_pcLEDs->SetAllColors(CColor::YELLOW);
+        }
+        else
+        {
+            CDisperseBehavior* pcDisperseBehavior = new CDisperseBehavior(0.1f, ToRadians(CDegrees(5.0f)));    // 0.1f reflects a distance of about 4.5cm
+            m_vecBehaviors.push_back(pcDisperseBehavior);
+
+            CHomingToFoodBeaconBehavior* pcHomingToFoodBeaconBehavior = new CHomingToFoodBeaconBehavior();
+            m_vecBehaviors.push_back(pcHomingToFoodBeaconBehavior);
+
+            CRandomWalkBehavior* pcRandomWalkBehavior = new CRandomWalkBehavior(0.05f);
+            m_vecBehaviors.push_back(pcRandomWalkBehavior);
+        }
+    }
+}
+
+/****************************************/
+/****************************************/
+
+void CEPuckHomSwarm::Reset()
+{
+    /* Set LED color */
+    m_pcLEDs->SetAllColors(CColor::WHITE);
+    m_pcRABA->ClearData();
+}
+
+/****************************************/
+/****************************************/
+
+/*
+ * This statement notifies ARGoS of the existence of the controller.
+ * It binds the class passed as first argument to the string passed as
+ * second argument.
+ * The string is then usable in the XML configuration file to refer to
+ * this controller.
+ * When ARGoS reads that string in the XML file, it knows which controller
+ * class to instantiate.
+ * See also the XML configuration files for an example of how this is used.
+ */
+REGISTER_CONTROLLER(CEPuckHomSwarm, "epuck_homswarm_controller")
---
+++ controllers/epuck_hom_swarm/epuck_hom_swarm.h
@@ -0,0 +1,197 @@
+/*
+ * AUTHOR: Danesh Tarapore <daneshtarapore@gmail.com>
+ *
+ * An example foraging controller for the foot-bot.
+ *
+ * This controller is meant to be used with the XML file:
+ *    experiments/foraging.argos
+ */
+
+//! TODO
+//! 1. Introduce noise in the sensors and actuators of the e-puck (see parameters from Lorenzo)
+
+
+#ifndef EPUCK_HOMSWARM_H
+#define EPUCK_HOMSWARM_H
+
+/*
+ * Include some necessary headers.
+ */
+
+#include <iostream>
+#include <vector>
+
+/* Definition of the CCI_Controller class. */
+#include <argos3/core/control_interface/ci_controller.h>
+
+/* Definition of the differential steering actuator */
+#include <argos3/plugins/robots/generic/control_interface/ci_differential_steering_actuator.h>
+
+/* Definition of the LEDs actuator */
+#include <argos3/plugins/robots/generic/control_interface/ci_leds_actuator.h>
+
+/* Definition of the range and bearing actuator */
+#include <argos3/plugins/robots/generic/control_interface/ci_range_and_bearing_actuator.h>
+
+/* Definition of the range and bearing sensor */
+#include <argos3/plugins/robots/generic/control_interface/ci_range_and_bearing_sensor.h>
+
+/* Definition of the foot-bot proximity sensor */
+#include <argos3/plugins/robots/e-puck/control_interface/ci_epuck_proximity_sensor.h>
+
+/* Definitions for random number generation */
+#include <argos3/core/utility/math/rng.h>
+
+
+/* Definitions for behaviors used to control robot */
+#include "behavior.h"
+#include "aggregatebehavior.h"
+#include "dispersebehavior.h"
+#include "randomwalkbehavior.h"
+#include "phototaxisbehavior.h"
+#include "antiphototaxisbehavior.h"
+#include "homingtofoodbeaconbehavior.h"
+#include "circlebehavior.h"
+
+/*
+ * All the ARGoS stuff in the 'argos' namespace.
+ * With this statement, you save typing argos:: every time.
+ */
+using namespace argos;
+
+/*
+ * A controller is simply an implementation of the CCI_Controller class.
+ */
+class CEPuckHomSwarm : public CCI_Controller
+{
+
+public:
+
+    struct ExperimentToRun
+    {
+        /* The type of experiment to run */
+        enum SwarmBehavior
+        {
+            SWARM_AGGREGATION = 0,
+            SWARM_DISPERSION,
+            SWARM_HOMING
+        } SBehavior;
+
+
+        /* The possible faults on robot */
+        enum FaultBehavior
+        {
+            FAULT_NONE = 0,
+
+            /*faults whose effects cause one of the following four general failures */
+            FAULT_STRAIGHTLINE,
+            FAULT_RANDOMWALK,
+            FAULT_CIRCLE,
+            FAULT_STOP
+        } FBehavior;
+
+
+        std::string id_FaultyRobotInSwarm;
+
+        ExperimentToRun();
+        void Init(TConfigurationNode& t_node);
+    };
+
+    /*
+    * The following variables are used as parameters for
+    * turning during navigation. You can set their value
+    * in the <parameters> section of the XML configuration
+    * file, under the
+    * <controllers><footbot_foraging_controller><parameters><wheel_turning>
+    * section.
+    */
+    struct SWheelTurningParams
+    {
+        Real MaxSpeed;
+        void Init(TConfigurationNode& t_tree);
+    };
+
+public:
+
+    /* Class constructor. */
+    CEPuckHomSwarm();
+    /* Class destructor. */
+    virtual ~CEPuckHomSwarm() {}
+
+    /*
+    * This function initializes the controller.
+    * The 't_node' variable points to the <parameters> section in the XML
+    * file in the <controllers><footbot_foraging_controller> section.
+    */
+    virtual void Init(TConfigurationNode& t_node);
+
+    /*
+    * This function is called once every time step.
+    * The length of the time step is set in the XML file.
+    */
+    virtual void ControlStep();
+
+    /*
+     * Execute general faults
+    */
+    virtual void RunGeneralFaults();
+
+    /*
+     * Run one of the swarm experiments (Aggregation, Dispersion, Homing)
+    */
+    virtual void RunHomogeneousSwarmExperiment();
+
+    /*
+    * This function resets the controller to its state right after the
+    * Init().
+    * It is called when you press the reset button in the GUI.
+    */
+    virtual void Reset();
+
+    /*
+    * Called to cleanup what done by Init() when the experiment finishes.
+    * In this example controller there is no need for clean anything up,
+    * so the function could have been omitted. It's here just for
+    * completeness.
+    */
+    virtual void Destroy() {}
+
+    /*
+    * Returns the experiment type
+    */
+    inline ExperimentToRun& GetExperimentType()
+    {
+        return m_sExpRun;
+    }
+
+private:
+
+
+
+private:
+
+    TBehaviorVector             m_vecBehaviors;
+    bool                        b_damagedrobot;     // true if robot is damaged
+
+    /* Pointer to the differential steering actuator */
+    CCI_DifferentialSteeringActuator* m_pcWheels;
+    /* Pointer to the LEDs actuator */
+    CCI_LEDsActuator* m_pcLEDs;
+    /* Pointer to the range and bearing actuator */
+    CCI_RangeAndBearingActuator*  m_pcRABA;
+    /* Pointer to the range and bearing sensor */
+    CCI_RangeAndBearingSensor* m_pcRABS;
+    /* Pointer to the foot-bot proximity sensor */
+    CCI_EPuckProximitySensor* m_pcProximity;
+
+    /* The random number generator */
+    CRandom::CRNG* m_pcRNG;
+
+
+    /*Information on the experiment to run - the swarm behavior and the faulty behavior*/
+    ExperimentToRun m_sExpRun;
+    /* The turning parameters */
+    SWheelTurningParams m_sWheelTurningParams;
+};
+
+#endif
